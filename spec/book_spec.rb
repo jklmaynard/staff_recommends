f0146a8d70e46ca2a0407c1dcfaf49e7f717a210
James Maynard <jameskirkmaynard@gmail.com>
2015-02-06 23:38:12 +0000

    add titleize method to Book class with passing spec.

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -5,4 +5,10 @@ describe(Book) do
   it { should have_and_belong_to_many(:employees) }
   it { should have_and_belong_to_many(:genres) }
   it { should have_many(:comments) }
+  
+  it("capitalizes each word of the title of the book") do
+    book = Book.create({ :name => "the book" })  
+    expect(book.name()).to eq ("The Book")
+  end
+  
 end
